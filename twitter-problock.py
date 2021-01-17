e96207364e270959fbb7d002e5876306ea7ab0b5
Martin Breuer <mail@martin-breuer.com>
2021-01-17 01:00:54 +0100

    login, start looking for promoted content

--- twitter-problock.py
+++ twitter-problock.py
@@ -4,12 +4,31 @@ scrape your twitter account for promoted content and block the source account
 """
 
 import secrets
-import mechanicalsoup
 
-fake_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0"
-url = "https://mobile.twitter.com/login"
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-browser = mechanicalsoup.StatefulBrowser(user_agent=fake_agent)
-browser.open(url)
+browser = webdriver.Firefox()
+browser.get(secrets.url)
+browser.implicitly_wait(3)
 
-print(browser.page)
+assert secrets.target in browser.title
+
+username_input = browser.find_element(By.NAME, 'session[username_or_email]')
+username_input.send_keys(secrets.username)
+
+password_input = browser.find_element(By.NAME, 'session[password]')
+password_input.send_keys(secrets.password + Keys.RETURN)
+
+timeline = WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.XPATH, "//div[@data-testid='primaryColumn']")))
+print(timeline)
+
+#timeline = browser.find_element_by_id('primaryColumn')
+#promoted_tweet = timeline.find_element_by_xpath("//*[contains(text(), 'Promoted')]")
+#print(promoted_tweet)
+
+print('!Successful Run')
+#browser.quit()
