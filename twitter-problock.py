8509e5de6f5a9b4f6edb4ee90c3f06aeff74af11
Martin Breuer <5493156+kautzz@users.noreply.github.com>
2021-03-10 22:18:49 +0100

    audio notification

--- twitter-problock.py
+++ twitter-problock.py
@@ -2,9 +2,13 @@
 twitter-problock (MVP)
 Scrape your twitter account for promoted content and block the source. You Pay - I Block!
 """
+#!/usr/bin/env python3
 
 import secrets
 import time
+import random
+
+import simpleaudio as sa
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -14,10 +18,15 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 
 # How many promoters should we block today
-block_target = 5
+block_target = 100
 
-browser = webdriver.Firefox()
 # TODO learn about browser settings
+profile = webdriver.FirefoxProfile()
+profile.set_preference("general.useragent.override", "Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1")
+browser = webdriver.Firefox(profile)
+browser.set_window_position(0, 0)
+browser.set_window_size(960, 1080)
+
 
 
 # Helper for waiting until page has loaded
@@ -44,11 +53,17 @@ def login():
     username_input.send_keys(secrets.username)
     password_input = browser.find_element(By.NAME, 'session[password]')
     password_input.send_keys(secrets.password + Keys.RETURN)
+    #time.sleep(60) # give me some time to enter the second factor
     print('[⚷] Logged In As User: ' + secrets.username)
     WebDriverWait(browser, 10).until(PageLoaded())
 
     # Wait for Tweets to load and select the timeline
     timeline = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.XPATH, "//div[@data-testid='primaryColumn']")))
+
+    wave_obj = sa.WaveObject.from_wave_file("notification.wav")
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
+
     print('===============================')
 
     return timeline
@@ -76,10 +91,13 @@ def block_user(promoted):
     browser.find_element(By.XPATH, "//div[@data-testid='confirmationSheetConfirm']").click()
     print('[✝] R.I.P')
 
+def rand_delay():
+    time.sleep(random.randint(0,4))
 
 def load_more_tweets():
 
-    print('[⬇] Scrolling To Lazy Load Tweets')
+    rand_delay()
+    #print('[⬇] Scrolling To Lazy Load Tweets')
     browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
     WebDriverWait(browser, 10).until(PageLoaded())
@@ -90,13 +108,13 @@ def load_more_tweets():
 
 def refresh_page():
 
-    print('[⟳] Reloading Page')
+    rand_delay()
+    #print('[⟳] Reloading Page')
     browser.refresh()
     WebDriverWait(browser, 10).until(PageLoaded())
     timeline = browser.find_element(By.XPATH, "//div[@data-testid='primaryColumn']")
 
     return timeline
-
 
 def main():
 
@@ -116,17 +134,19 @@ def main():
             print('[☭] Blocked ' + str(blocked_users) + '/' + str(block_target) + ' Promoters')
             print('===============================')
 
-
+            wave_obj = sa.WaveObject.from_wave_file("notification.wav")
+            play_obj = wave_obj.play()
+            play_obj.wait_done()
             timeline = refresh_page()
             lazy_loads = 0
 
-        elif lazy_loads < 3:
-            print('[⚲] No Promoted Content Found In Timeline')
+        elif lazy_loads < 4:
+            #print('[⚲] No Promoted Content Found In Timeline')
             timeline = load_more_tweets()
             lazy_loads += 1
 
         else:
-            print('[⚲] No Promoted Content Found In Timeline')
+            #print('[⚲] No Promoted Content Found In Timeline')
             timeline = refresh_page()
             lazy_loads = 0
 
@@ -135,4 +155,4 @@ if __name__ == "__main__":
     main()
 
 print('[☑] End Of Program')
-#browser.quit()
+browser.quit()
